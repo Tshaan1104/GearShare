ce2c883916be3859f0f1df6bb1ccc9938bbd74a5
Tshaan1104 <tshaan1104@gmail.com>
2024-07-28 23:12:19 +0530

    supabase configuration

--- RentMyRide/app/lib/supabase.ts
+++ RentMyRide/app/lib/supabase.ts
@@ -10,3 +10,5 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
+
+export default supabase;
